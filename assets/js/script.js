c5d7911e5860d7af426489ab9f33abf8668f4720
Brian Otieno <bryomajor@gmail.com>
2019-08-18 00:44:21 +0300

    Integrate business logice to the the user logic. Also style the answer container.

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,13 +1,16 @@
-var DD = Number(prompt("Enter day:"));
-var MM = Number(prompt("Enter month:"));
-var CC = Number(prompt("Enter century:"));
-var YY = Number(prompt("Enter year"));
+// Get the inputed values
+var dayOfBirth = Number(document.getElementById("day-of-birth").value);
+var monthOfBirth = Number(document.getElementById("month-of-birth").value);
+var inputedYear = [document.getElementById("year-of-birth").value];
+var centuryOfBirth = Number(inputedYear.slice(0,2));
+var yearOfBirth = Number(inputedYear.slice(2,4));
 
-/*function maleAkanName () {
-		var d = Math.round(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
+// Calculate male Akan name
+function maleAkanName () {
+	var dayIndex = Math.round(( ( (centuryOfBirth/4) -2*centuryOfBirth-1) + ((5*yearOfBirth/4) ) + ((26*(monthOfBirth+1)/10)) + dayOfBirth) % 7);
     var akanName;
     
-    switch(d) {
+    switch(dayIndex) {
     		case 0:
         akanName = "Kwasi";
         alert("Kwasi");
@@ -42,45 +45,63 @@ var YY = Number(prompt("Enter year"));
     }
 }
 
-maleAkanName();*/
 
+// Calculate female Akan name
 function femaleAkanName () {
-    var d = Math.round(( ( (CC/4) -2*CC-1) + ((5*YY/4) ) + ((26*(MM+1)/10)) + DD ) % 7);
-var akanName;
+    var dayIndex = Math.round(( ( (centuryOfBirth/4) -2*centuryOfBirth-1) + ((5*yearOfBirth/4) ) + ((26*(monthOfBirth+1)/10)) + dayOfBirth ) % 7);
+    var akanName;
 
-switch(d) {
+    switch(dayIndex) {
         case 0:
-    akanName = "Akosua";
-    alert("Akosua");
-    break;
-    case 1:
-    akanName = "Adwoa";
-    alert("Adwoa");
-    break;
-    case 2:
-    akanName = "Abenaa";
-    alert("Abenaa");
-    break;
-    case 3:
-    akanName = "Akua";
-    alert("Akua");
-    break;
-    case 4:
-    akanName = "Yaa";
-    alert("Yaa");
-    break;
-    case 5:
-    akanName = "Afua";
-    alert("Afua");
-    break;
-    case 6:
-    akanName = "Ama";
-    alert("Ama");
-    break;
-    default:
-    akanName = "Please input the correct details!"
-    alert("Please input the correct details!")
-}
+        akanName = "Akosua";
+        alert("Akosua");
+        break;
+        case 1:
+        akanName = "Adwoa";
+        alert("Adwoa");
+        break;
+        case 2:
+        akanName = "Abenaa";
+        alert("Abenaa");
+        break;
+        case 3:
+        akanName = "Akua";
+        alert("Akua");
+        break;
+        case 4:
+        akanName = "Yaa";
+        alert("Yaa");
+        break;
+        case 5:
+        akanName = "Afua";
+        alert("Afua");
+        break;
+        case 6:
+        akanName = "Ama";
+        alert("Ama");
+        break;
+        default:
+        akanName = "Please input the correct details!"
+        alert("Please input the correct details!")
+    }
 }
 
-femaleAkanName();
+// Convert the calculate Akan name functions into variables
+var femaleChosen = femaleAkanName();
+var maleChosen = maleAkanName();
+
+// Gets the chosen checkbox and runs one of the calculate Akan name functions
+function yourSubmission() {
+    // Get the checkbox
+    var female = document.getElementById("females");
+    var male = document.getElementById("males");
+  
+    // If the female checkbox is checked, run the female function || male checkbox is checked, run the male function
+    if (female.checked == true){
+      alert(femaleChosen);
+    } else {
+      alert(maleChosen);
+    }
+  }
+
+  document.getElementById("answerArea").innerHTML = yourSubmission();
