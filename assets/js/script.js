90e64c37421029205c534d4b3aa80f435dea0769
Brian Otieno <bryomajor@gmail.com>
2019-08-21 17:55:41 +0300

    Update formula to avoid using century.

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,17 @@ function main () {
 	var month = document.getElementById("month-of-birth").value;
 	var year = document.getElementById("year-of-birth").value;
 	// Getting CC and YY
-	var centuryCentury = parseInt(year.slice(0,2));
-	var yearYear = parseInt(year.slice(2,4));
+	// var centuryCentury = parseInt(year.slice(0,2));
+	// var yearYear = parseInt(year.slice(2,4));
 
-	var dayOfTheWeek = Math.round( ( ( (centuryCentury/4) -2*centuryCentury-1) + ((5*yearYear/4) ) + ((26*(month+1)/10)) + day ) % 7);
+	var a = Math.floor((14 - month) / 12);
+    var y = year - a;
+    var m = month + 12 * a - 2;
+    dayOfTheWeek = (day + y + Math.floor(y / 4) - Math.floor(y / 100) +
+        Math.floor(year / 400) + Math.floor((31 * m) / 12)) % 7;
+	// var dayOfTheWeek = Math.round( ( ( (centuryCentury/4) -2*centuryCentury-1) + ((5*yearYear/4) ) + ((26*(month+1)/10)) + day ) % 7);
 
+	// alert(dayOfTheWeek);
 	maleAkanNames = ["Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"];
 	femaleAkanNames = ["Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"];
 
@@ -17,15 +23,16 @@ function main () {
 	} else {
 		var gender = 'female';
 	}
+	// alert(gender);
 
 	if(day < 1 || day > 31) {
 		alert("You have entered an invalid date!");
 	}
-	else if(month < 1 || month > 12) {
+	else if(month < 1 || month > 13) {
 		alert("You have entered an invalid month!")
 	}
-	else if(year < 1900 || year > 2099) {
-		alert("Enter year between 1900 - 2099!")
+	else if(year.length!=4) {
+		alert("Invalid Year")
 	}
 	else if(gender === 'male' && dayOfTheWeek === 0) {
 		alert("Your Akan name is " + maleAkanNames[0]);
@@ -70,7 +77,7 @@ function main () {
 		alert("Your Akan name is " + femaleAkanNames[6]);
 	}
 	else {
-		alert("Please try again with the correct details!")
+		alert("Error processing your akan name!")
 	}
 	
 }
